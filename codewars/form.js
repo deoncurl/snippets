b643f66417e91acc7a219ab5f6d16d7caeb07da2
Fbasham <48450599+Fbasham@users.noreply.github.com>
2021-02-20 09:08:43 -0500

    Update form.js

--- codewars/form.js
+++ codewars/form.js
@@ -1,7 +1,7 @@
 form = document.querySelector("form");
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  console.log(e.target[0].value);
-  console.log(e.target[1].value);
+  console.log(e.target.user.value);
+  console.log(e.target.pass.value);
   form.reset();
 });
